8db9f28ad7f236007c5c6aeb05aa00702893dcbd
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-11-19 15:47:51 +0000

    Profile component added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile(props){
+    return(
+        <>
+        <table>
+            <tr>
+                <td rowSpan={7}>
+                    <img src={require('../assests/img/1.jpg')} alt="profile picture" />
+                </td>
+                <td>ID</td>
+            </tr>
+        </table>
+        </>
+    );
+}
